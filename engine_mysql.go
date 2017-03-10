bf5dc4d67901ef875cbfd2066792b9177fb2ccb2
Nevidimka <deadkrolik@gmail.com>
2017-03-10 11:28:27 +0500

    mysql-related code moved to engine struct

---
+++ engine_mysql.go
@@ -0,0 +1,110 @@
+package migrator
+
+import (
+	"database/sql"
+	//custom engine
+	_ "github.com/go-sql-driver/mysql"
+	"strings"
+)
+
+//EngineMysql - engine to execute queries in MySQL
+type EngineMysql struct {
+	dsn        string
+	connection *sql.DB
+	table      string
+}
+
+//GetEngineMysql - get engine object with connection config like "user:pass@host/dbname?charset=utf8"
+func GetEngineMysql(dsn string) *EngineMysql {
+	return &EngineMysql{dsn: dsn}
+}
+
+const (
+	migrationsMysql = "CREATE TABLE {TABLE} (" +
+		"`id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY," +
+		"`hash` varchar(32) NOT NULL," +
+		"`statement` varchar(600) NOT NULL," +
+		"`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP" +
+		") ENGINE=InnoDB DEFAULT CHARSET=utf8"
+)
+
+//PrepareConnection - init connection and migrations table
+func (engine *EngineMysql) PrepareConnection(tableName string) error {
+	engine.table = tableName
+
+	conn, err := sql.Open("mysql", engine.dsn)
+	if err != nil {
+		return err
+	}
+	engine.connection = conn
+
+	isExists, err := engine.isMigrationsTableExists()
+	if err != nil {
+		return err
+	}
+
+	if !isExists {
+		err := engine.ExecQuery(strings.Replace(migrationsMysql, "{TABLE}", engine.table, 1))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//ExecQuery - execute one query
+func (engine *EngineMysql) ExecQuery(query string) error {
+	_, err := engine.connection.Exec(query)
+
+	return err
+}
+
+//IsMigrationApplied - is migration already executed (pass query md5-hash as parameter)
+func (engine *EngineMysql) IsMigrationApplied(hash string) (bool, error) {
+	var count int
+
+	err := engine.connection.QueryRow(
+		"SELECT COUNT(*) AS m_count FROM "+engine.table+" WHERE hash = ?",
+		hash,
+	).Scan(&count)
+
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
+}
+
+//RegisterMigration - add new migration record to table
+func (engine *EngineMysql) RegisterMigration(hash, query string) error {
+	prepared, err := engine.connection.Prepare(
+		"INSERT INTO " + engine.table + " SET hash = ?, statement = ?",
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = prepared.Exec(hash, query)
+	return err
+}
+
+//Disconnect - do a cleanup
+func (engine *EngineMysql) Disconnect() {
+	_ = engine.connection.Close()
+}
+
+//isMigrationsTableExists - if migrations table exists
+func (engine *EngineMysql) isMigrationsTableExists() (bool, error) {
+	var exist string
+
+	err := engine.connection.QueryRow("SHOW TABLES LIKE '" + engine.table + "'").Scan(&exist)
+	if err == sql.ErrNoRows {
+		return false, nil
+	}
+	if err != nil {
+		return false, err
+	}
+
+	return exist == engine.table, nil
+}
--- migrator.go
+++ migrator.go
@@ -2,16 +2,21 @@ package migrator
 
 import (
 	"crypto/md5"
-	"database/sql"
 	"fmt"
-	//custom engine
-	_ "github.com/go-sql-driver/mysql"
 )
 
 //StartUpMigrator - migrations, built-in the app
 type StartUpMigrator struct {
-	connectionConfig string
-	connection       *sql.DB
+	engine Engine
+}
+
+//Engine - specific database driver
+type Engine interface {
+	PrepareConnection(dsn string) error
+	ExecQuery(query string) error
+	IsMigrationApplied(hash string) (bool, error)
+	RegisterMigration(hash, query string) error
+	Disconnect()
 }
 
 //StatementResult - migration query result
@@ -22,23 +27,11 @@ type StatementResult struct {
 	ErrorMessage string
 }
 
-const (
-	migrationsTableName   = "strtp_mmigrator"
-	migrationsTableCreate = "CREATE TABLE strtp_mmigrator (" +
-		"`id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY," +
-		"`hash` varchar(32) NOT NULL," +
-		"`statement` varchar(600) NOT NULL," +
-		"`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP" +
-		") ENGINE=InnoDB DEFAULT CHARSET=utf8"
-)
+//GetMigrator - get new migrator object
+func GetMigrator(tableName string, engine Engine) (*StartUpMigrator, error) {
+	migrator := StartUpMigrator{engine: engine}
 
-//GetMigrator - get new migrator object with connect string "user:pass@/DBNAME?charset=utf8"
-func GetMigrator(config string) (*StartUpMigrator, error) {
-	migrator := StartUpMigrator{
-		connectionConfig: config,
-	}
-
-	err := migrator.prepare()
+	err := migrator.engine.PrepareConnection(tableName)
 	if err != nil {
 		return nil, err
 	}
@@ -46,44 +39,35 @@ func GetMigrator(config string) (*StartUpMigrator, error) {
 	return &migrator, nil
 }
 
-//Run - run queries execution
-func (migrator *StartUpMigrator) Run(statements []string) []StatementResult {
+//Run - start migrations execution. Returns executed statements and general error
+func (migrator *StartUpMigrator) Run(statements []string) ([]StatementResult, error) {
 
 	var result []StatementResult
-	defer migrator.connection.Close()
+	defer migrator.engine.Disconnect()
 
 	for _, statement := range statements {
 
 		//if it's already applied
-		hash := fmt.Sprintf("%x", md5.Sum([]byte(statement)))
-
-		isApplied, err := migrator.isMigrationApplied(hash)
+		hash := migrator.getHash(statement)
+		isApplied, err := migrator.engine.IsMigrationApplied(hash)
 		if err != nil {
-			return migrator.returnErrorStatement(result, statement, err)
+			return migrator.getErrorStatement(result, statement, err)
 		}
 
 		if isApplied {
 			continue
 		}
 
-		//exec query
-		_, err = migrator.connection.Exec(statement)
-
 		//something went wrong
+		err = migrator.engine.ExecQuery(statement)
 		if err != nil {
-			return migrator.returnErrorStatement(result, statement, err)
+			return migrator.getErrorStatement(result, statement, err)
 		}
 
 		//register new migration
-		prepared, err := migrator.connection.Prepare(
-			"INSERT INTO " + migrationsTableName + " SET hash = ?, statement = ?",
-		)
+		err = migrator.engine.RegisterMigration(hash, statement)
 		if err != nil {
-			return migrator.returnErrorStatement(result, statement, err)
-		}
-		_, err = prepared.Exec(hash, statement)
-		if err != nil {
-			return migrator.returnErrorStatement(result, statement, err)
+			return migrator.getErrorStatement(result, statement, err)
 		}
 
 		result = append(result, StatementResult{
@@ -93,78 +77,22 @@ func (migrator *StartUpMigrator) Run(statements []string) []StatementResult {
 		})
 	}
 
-	return result
+	return result, nil
 }
 
 //returnErrorStatement - add error query to result
-func (migrator *StartUpMigrator) returnErrorStatement(result []StatementResult, query string, err error) []StatementResult {
-
+func (migrator *StartUpMigrator) getErrorStatement(result []StatementResult, query string, err error) ([]StatementResult, error) {
 	result = append(result, StatementResult{
 		IsSuccess:    false,
 		Statement:    query,
 		Hash:         migrator.getHash(query),
 		ErrorMessage: err.Error(),
 	})
-	return result
+
+	return result, err
 }
 
 //getHash - hash of query string
 func (migrator *StartUpMigrator) getHash(query string) string {
 	return fmt.Sprintf("%x", md5.Sum([]byte(query)))
-}
-
-//prepare - init connection and migrations table
-func (migrator *StartUpMigrator) prepare() error {
-
-	conn, err := sql.Open("mysql", migrator.connectionConfig)
-	if err != nil {
-		return err
-	}
-	migrator.connection = conn
-
-	isExists, err := migrator.isMigrationsTableExists()
-	if err != nil {
-		return err
-	}
-
-	if !isExists {
-		_, err := migrator.connection.Exec(migrationsTableCreate)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-//isMigrationApplied - is migration already executed
-func (migrator *StartUpMigrator) isMigrationApplied(hash string) (bool, error) {
-
-	var count uint64
-
-	err := migrator.connection.QueryRow(
-		"SELECT COUNT(*) AS m_count FROM "+migrationsTableName+" WHERE hash = ?", hash,
-	).Scan(&count)
-
-	if err != nil {
-		return false, err
-	}
-
-	return count > 0, nil
-}
-
-//isMigrationsTableExists - if migrations table exists
-func (migrator *StartUpMigrator) isMigrationsTableExists() (bool, error) {
-
-	var exist string
-
-	err := migrator.connection.QueryRow("SHOW TABLES LIKE '" + migrationsTableName + "'").Scan(&exist)
-	if err == sql.ErrNoRows {
-		return false, nil
-	}
-	if err != nil {
-		return false, err
-	}
-
-	return exist == migrationsTableName, nil
 }
